dbf57843c958455b0ac5bfd87f0ab3602e58829e
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-27 14:14:03 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("sumFirstAndLastDigit!");
+
+        System.out.println(sumFirstAndLastDigit (252));
+        System.out.println(sumFirstAndLastDigit (257));
+        System.out.println(sumFirstAndLastDigit (0));
+        System.out.println(sumFirstAndLastDigit (5));
+        System.out.println(sumFirstAndLastDigit (-10));
     }
+
+    public static int sumFirstAndLastDigit(int number){
+        if(number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+        int lastDigit = number % 10;
+        int firstDigit = 0;
+
+        while (number >= 10) {
+            number /= 10;
+        }
+        firstDigit = number;
+        sum = firstDigit + lastDigit;
+        return sum;
+    }
+
+
 }
